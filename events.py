6118ef764344d85778eb7274883f4ec16741dcbb
abdulayo2987 <ramennoodles2006@outlook.com>
2025-02-11 18:26:37 +0000

    fixed stuff

--- events.py
+++ events.py
@@ -1,1 +1,25 @@
 from moderation import *
+from datetime import datetime
+
+file = "nea.sqlite"
+
+def event_commands():
+
+    @bot.command(name="new_event", description="creates a new event")
+    @app_commands.describe(event="what would you like to call the event",  date="what date is the event", time="what time is the event", location="where is the event")
+    async def new_event(interaction: discord.Interaction, event: str, date: str, time: str, location: str):
+        pass
+
+    @bot.command(name="delete_event", description="deletes an event")
+    @app_commands.describe(event="name of the event you want to remove")
+    async def new_event(interaction: discord.Interaction, ):#options for all current events
+        pass
+
+    @bot.command(name="edit_event", description="changes a part of an existing event")
+    @app_commands.describe(event="name of the event you want to change", date="what date is the new event", time="what time is the new event")
+    async def edit_event(interaction: discord.Interaction, event: str, date: str, time: str):
+        pass
+
+    @bot.command(name="list_event", description="lists all upcoming events")
+    async def list_event(interaction: discord.Interaction):
+        pass
---
+++ music.py
@@ -0,0 +1,7 @@
+from moderation import *
+from ytmusicapi import YTMusic
+
+ytmusic = YTMusic()
+
+def search_music(song:str, artist:str = None):
+    pass
